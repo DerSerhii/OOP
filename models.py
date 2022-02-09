9e076932abd82e0e21593a424b2d3b29727e41ce
DerSerhii <proektssd@gmail.com>
2022-02-09 19:41:25 +0200

    optimized

--- models.py
+++ models.py
@@ -2,34 +2,29 @@ import datetime as dt
 
 
 class Employee:
+	TEXT_COMPARISON = "comparison by salary per day is"
 
 	def __init__(self, full_name, salary_day):
 		self.full_name = full_name
 		self.salary_day = salary_day
 
 	def __lt__(self, other):
-		salary_day = self.salary_day < other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day < other.salary_day}"
 
 	def __le__(self, other):
-		salary_day = self.salary_day <= other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day <= other.salary_day}"
 
 	def __gt__(self, other):
-		salary_day = self.salary_day > other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day > other.salary_day}"
 
 	def __ge__(self, other):
-		salary_day = self.salary_day >= other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day >= other.salary_day}"
 
 	def __eq__(self, other):
-		salary_day = self.salary_day == other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day == other.salary_day}"
 
 	def __ne__(self, other):
-		salary_day = self.salary_day != other.salary_day
-		return f"by salary per day comparison is {salary_day}"
+		return f"{self.TEXT_COMPARISON} {self.salary_day != other.salary_day}"
 
 	@staticmethod
 	def work():
@@ -37,7 +32,7 @@ class Employee:
 
 	@staticmethod
 	def calculation_business_day():
-		""" Calculation of business days including the current day from the beginning of the month """
+		"""Calculation of business days including the current day from the beginning of the month"""
 
 		current_day = dt.date.today()
 		days, bus_days = current_day.day, 0
@@ -57,11 +52,14 @@ class Employee:
 		return bus_days
 
 	def check_salary(self, amount_day: int, current_salary: str = None):
-		""" Default: defines the salary for the amount of days
-			Passing the second argument "cur": defines the salary at the current moment from the beginning of the month
 		"""
+		Default: defines the salary for the amount of days
+		Passing the second argument "cur": defines the salary at the current moment from the beginning of the month
+		"""
+
 		if current_salary == "cur":
 			return self.salary_day * self.calculation_business_day()
+
 		return amount_day * self.salary_day
 
 
@@ -76,6 +74,8 @@ class Recruiter(Employee):
 
 
 class Programmer(Employee):
+	TEXT_COMPARISON_PROG = "comparison by amount of applied technologies is"
+	__count = 0
 
 	def __init__(self, full_name, salary_day, tech_stack):
 		super().__init__(full_name, salary_day)
@@ -85,54 +85,50 @@ class Programmer(Employee):
 		return f"{self.__class__.__name__}: {self.full_name}"
 
 	def __add__(self, other):
-		if not isinstance(other, Programmer):
-			return NotImplemented
-		name = 'Alfa' + self.full_name + other.full_name
-		salary_day = self.salary_day + other.salary_day
-		tech_stack = ', '.join(sorted(set(self.tech_stack.split(', ') + other.tech_stack.split(', '))))
+		Programmer.__count += 1
+		try:
+			name = f"AlfaProgrammer{Programmer.__count}"
+			salary_day = self.salary_day + other.salary_day
+			tech_stack = ', '.join(sorted(set(self.tech_stack.split(', ') + other.tech_stack.split(', '))))
+		except AttributeError:
+			return 'Attention!! Racial discrimination!! You can synthesize alpha programmers only among programmers!!'
 		return Programmer(name, salary_day, tech_stack)
 
 	def __lt__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day < other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) < len(other.tech_stack.split(", "))
-		return f"by the salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__lt__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	def __le__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day <= other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) <= len(other.tech_stack.split(", "))
-		return f"by salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__le__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	def __gt__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day > other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) > len(other.tech_stack.split(", "))
-		return f"by salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__gt__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	def __ge__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day >= other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) >= len(other.tech_stack.split(", "))
-		return f"by salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__ge__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	def __eq__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day == other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) == len(other.tech_stack.split(", "))
-		return f"by salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__eq__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	def __ne__(self, other):
 		if not isinstance(other, Programmer):
 			return NotImplemented
-		salary_day = self.salary_day != other.salary_day
 		tech_stack = len(self.tech_stack.split(", ")) != len(other.tech_stack.split(", "))
-		return f"by salary per day comparison is {salary_day} \nby the number of applied technologies comparison: {tech_stack}"
+		return f"{super().__ne__(other)}\n{self.TEXT_COMPARISON_PROG} {tech_stack}"
 
 	@staticmethod
 	def work():
@@ -155,5 +151,3 @@ class Vacancy:
 		self.title = title
 		self.main_skill = main_skill
 		self.main_skill_level = main_skill_level
-
-
